4584186363124c55e5326ca11ad6bd2c1e95883e
unknown <Administrator@PNVD191.pn.com.vn>
2021-01-15 11:43:02 +0700

    model cua a Vinh redux

---
+++ index.js
@@ -0,0 +1,10 @@
+import App from './src/Setup';
+
+// messaging()
+//   .getIsHeadless()
+//   .then(isHeadless => {
+//     console.log('isHeadless', isHeadless);
+//     // do sth with isHeadless
+//   });
+
+App();
